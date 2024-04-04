db559b93ac11e52dd098d2d1b5310e6882d1ee0a
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-04-04 18:50:59 +0400

    removed password confirmation during signup

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,13 @@ from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('password2')
 
-    class Meta:
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ("email",)
+        fields = ("email", "password1")
 
 
 class CustomUserChangeForm(UserChangeForm):
