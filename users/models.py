f346cc00794684a4c4e22dcd3dbd9ca981f2c596
Irakli Skhirtladze <iskhirtladzei@gmail.com>
2024-04-09 10:56:46 +0400

    added verbose names

--- users/models.py
+++ users/models.py
@@ -9,10 +9,10 @@ from store.models import Book
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(_("email address"), unique=True)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-    date_joined = models.DateTimeField(default=timezone.now)
+    email = models.EmailField(verbose_name=_("Email address"), unique=True)
+    is_staff = models.BooleanField(default=False, verbose_name=_("Is staff?"))
+    is_active = models.BooleanField(default=True, verbose_name=_("Is active?"))
+    date_joined = models.DateTimeField(default=timezone.now, verbose_name=_("Date joined"))
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
@@ -25,9 +25,9 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
 # Create a many-to-many relationship cart model to store items
 class Cart(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    books = models.ManyToManyField(Book, related_name="cart")
-    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, verbose_name=_("User"))
+    books = models.ManyToManyField(Book, related_name="cart", verbose_name=_("Books"))
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00, verbose_name=_("Total"))
 
     def __str__(self):
         return f"Cart for {self.user.email}"
